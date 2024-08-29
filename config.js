992c4677fc592848c1e9b7326ec6cbb03afaeba8
Nguyen Dinh Quang <20010818@st.phenikaa-uni.edu.vn>
2024-08-29 10:53:46 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,19 @@
+const CONFIG = {
+    introTitle: 'Hoàiii ơiiiii!',
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao bà cứ đi nhầm đường
+    Lạc vào tim tôi lẻ loi
+    Đằng sau chữ yêu đây là thương`,
+    btnIntro: 'hihi',
+    title: 'Phải chăng Hoàiii đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng Hoàiii đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Vẫn cứ là thích ông <33',
+    btnNo: 'Không, ông già à :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao bà lại yêu tui <3',
+    btnReply: 'Gửi cho ông <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'ngại chớt ngại chớt. Yêu thế nhờ  😘😘',
+    messDesc: 'Tối nấu cơm cho tớ nhé.',
+    btnAccept: 'Okiiiii lun <3',
+   
+}
